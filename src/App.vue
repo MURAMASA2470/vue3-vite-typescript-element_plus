033a0a478e58fcfddab293d1fe405ee9d7e6fd17
MURAMASA2470 <keisuke226214@gmail.com>
2023-12-25 10:52:08 +0900

    適当なコンポーネントを貼り付けて確認

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,110 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="common-layout">
+    <el-container style="padding: 0;">
+      <el-header>Header</el-header>
+      <el-container style="padding: 0;" id="container">
+        <el-aside>Aside</el-aside>
+        <el-main>
+          <el-row class="row">
+            <el-col
+              v-for="(o) in 4"
+              :key="o"
+              :span="6"
+              class="col"
+            >
+              <el-card :body-style="{ padding: '0px' }">
+                <img
+                  src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                  class="image"
+                />
+                <div style="padding: 14px">
+                  <span>Yummy hamburger</span>
+                  <div class="bottom">
+                    <el-button text class="button">Operating</el-button>
+                  </div>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
+
+          <el-form :model="form" label-width="120px">
+            <el-form-item class="row" label="Activity name">
+              <el-input v-model="form.name" />
+            </el-form-item>
+            <el-form-item class="row" label="Activity zone">
+              <el-select v-model="form.region" placeholder="please select your zone">
+                <el-option label="Zone one" value="shanghai" />
+                <el-option label="Zone two" value="beijing" />
+              </el-select>
+            </el-form-item>
+            <el-form-item class="row" label="Activity time">
+              <el-col :span="11">
+                <el-date-picker
+                  v-model="form.date1"
+                  type="date"
+                  placeholder="Pick a date"
+                  style="width: 100%"
+                />
+              </el-col>
+              <el-col :span="2" class="text-center">
+                <span class="text-gray-500">-</span>
+              </el-col>
+              <el-col :span="11">
+                <el-time-picker
+                  v-model="form.date2"
+                  placeholder="Pick a time"
+                  style="width: 100%"
+                />
+              </el-col>
+            </el-form-item>
+            <el-form-item class="row" label="Instant delivery">
+              <el-switch v-model="form.delivery" />
+            </el-form-item>
+            <el-form-item class="row" label="Activity type">
+              <el-checkbox-group v-model="form.type">
+                <el-checkbox label="Online activities" name="type" />
+                <el-checkbox label="Promotion activities" name="type" />
+                <el-checkbox label="Offline activities" name="type" />
+                <el-checkbox label="Simple brand exposure" name="type" />
+              </el-checkbox-group>
+            </el-form-item>
+            <el-form-item class="row" label="Resources">
+              <el-radio-group v-model="form.resource">
+                <el-radio label="Sponsor" />
+                <el-radio label="Venue" />
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item class="row" label="Activity form">
+              <el-input v-model="form.desc" type="textarea" />
+            </el-form-item>
+            <el-form-item class="row" >
+              <el-button type="primary" @click="onSubmit">Create</el-button>
+              <el-button>Cancel</el-button>
+            </el-form-item>
+          </el-form>
+
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!')
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+</script>
